4a020cf782ec2e6201a821ca7c5dffe3e2f16a2a
Yuriy Bezgachyuk <yurkovskiy@gmail.com>
2016-03-10 14:51:48 +0200

    Added transclude example

--- directives/app_06.js
+++ directives/app_06.js
@@ -1,8 +1,21 @@
 var app = angular.module("myapp", []);
-app.directive("myDir", function() {
+app.directive("mydir", function() {
 	return {
 		restrict: "EA",
-		template: "<div>My Directive Template</div>",
+		template: "<div>mydir Template</div>",
 		replace: true
 	}
+});
+
+app.directive("mytdir", function() {
+	return {
+		restrict: "EA",
+		transclude: true,
+		template: "<div>mytdir template</div><div ng-transclude></div>",
+	}
+});
+
+
+app.controller("AppController", function($scope) {
+	$scope.value = "[value] from controller";
 });
